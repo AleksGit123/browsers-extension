52063595517bec3719062481d47b1ee53a9562ff
AleksGit123 <aleksichokheli03@gmail.com>
2025-04-01 11:43:43 +0400

    updated

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ themeChange.addEventListener("click",()=>{
  
     if(!document.body.classList.contains("dark_mode")){
         console.log(true);
-        document.body.classList.add("dark_mode");
+        // document.body.classList.add("dark_mode");
+        func(document.body,"dark_mode","add");
         
         // icon switch
         moon.style.display = "none";
@@ -25,13 +26,25 @@ themeChange.addEventListener("click",()=>{
     }
     else{
         console.log(false);
-        document.body.classList.remove("dark_mode");
+        // document.body.classList.remove("dark_mode");
+        func(document.body,"dark_mode","remove");
         sun.style.display = "none";
         moon.style.display = "block";
         themeChange.style.backgroundColor = "#eeefeb";
     }
 
 })
+// animation add and remove
+
+let func = function(element,classname,action = "add"){
+    if(action === "remove"){
+        element.classList.remove(classname);
+    }
+    else{
+        element.classList.add(classname);
+    }
+
+}
 
 toggle.forEach((element,index) =>{
     
@@ -43,16 +56,20 @@ toggle.forEach((element,index) =>{
         // console.log("circle index" + index)
        
         if(!currentCircle.classList.contains("animation_on")){
-            currentCircle.classList.add("animation_on");
+            // currentCircle.classList.add("animation_on");
+            func(currentCircle,"animation_on","add");
             activeToggles.push(index);
-            currentCircle.classList.remove("animation_off");
+            // currentCircle.classList.remove("animation_off");
+            func(currentCircle,"animation_off","remove");
             element.style.backgroundColor = "#D92626";
 
         }
         else{
             activeToggles = activeToggles.filter(i => i !==index)
-            currentCircle.classList.remove("animation_on");
-            currentCircle.classList.add("animation_off");
+            // currentCircle.classList.remove("animation_on");
+            func(currentCircle,"animation_on","remove");
+            // currentCircle.classList.add("animation_off");
+            func(currentCircle,"animation_off","add");
             element.style.backgroundColor = "#c5c5c5";
         }
         
@@ -61,29 +78,38 @@ toggle.forEach((element,index) =>{
 
 })
 
-
+let bgColor = function(color1,color2,color3){
+    all.style.backgroundColor = color1;
+    active.style.backgroundColor = color2;
+    inactive.style.backgroundColor = color3;
+}
 
 all.addEventListener("click",()=>{
-    all.style.backgroundColor = "#D92626";
-    active.style.backgroundColor = "#FFFFFF";
-    inactive.style.backgroundColor = "#FFFFFF";
+    // all.style.backgroundColor = "#D92626";
+    // active.style.backgroundColor = "#FFFFFF";
+    // inactive.style.backgroundColor = "#FFFFFF";
+    bgColor("#D92626","#FFFFFF","#FFFFFF")
     
-    extensions.forEach(element =>{
-        element.classList.remove("hide");
+    extensions.forEach(tag =>{
+        // element.classList.remove("hide");
+        func(tag,"hide","remove");
     })
 })
 
 active.addEventListener("click",() =>{
-    all.style.backgroundColor = "#FFFFFF";
-    active.style.backgroundColor = "#D92626";
-    inactive.style.backgroundColor = "#FFFFFF";
+    // all.style.backgroundColor = "#FFFFFF";
+    // active.style.backgroundColor = "#D92626";
+    // inactive.style.backgroundColor = "#FFFFFF";
+    bgColor("#FFFFFF","#D92626","#FFFFFF")
 
     console.log(activeToggles)
     extensions.forEach((element,index)=>{
-        element.classList.remove("hide");
+        // element.classList.remove("hide");
+        func(element,"hide","remove");
         if(!activeToggles.includes(index)){
             console.log(index);
-            element.classList.add("hide");
+            // element.classList.add("hide");
+            func(element,"hide","add");
         }
         
     })
@@ -93,17 +119,20 @@ active.addEventListener("click",() =>{
 inactive.addEventListener("click",()=>{
     
     extensions.forEach((element,index)=>{
-        element.classList.remove("hide");
+        // element.classList.remove("hide");
+        func(element,"hide","remove");
         if(activeToggles.includes(index)){
            
             console.log(index);
-            element.classList.add("hide");
+            // element.classList.add("hide");
+            func(element,"hide","add");
         }
         
     })
-    all.style.backgroundColor = "#FFFFFF";
-    active.style.backgroundColor = "#FFFFFF";
-    inactive.style.backgroundColor = "#D92626";
+    // all.style.backgroundColor = "#FFFFFF";
+    // active.style.backgroundColor = "#FFFFFF";
+    // inactive.style.backgroundColor = "#D92626";
+    bgColor("#FFFFFF","#FFFFFF","#D92626")
     
 })
 
@@ -113,7 +142,8 @@ remove.forEach((removeButton,index)=>{
         let currentArticle = extensions[index]
         console.log("article index: " + index);
         
-        currentArticle.classList.add("hide");
+        // currentArticle.classList.add("hide");
+        func(currentArticle,"hide","add");
         currentArticle.remove();
     })
     
